12034fb87651dc93162fd7e668b8e92c897d96fc
仓木熏啊 <>
2025-11-03 11:08:51 +0800

    fix:限制范围

--- src/components/cameraManager.js
+++ src/components/cameraManager.js
@@ -77,6 +77,7 @@ export class CameraManager {
     this.controls = new OrbitControls(this.camera, renderer.domElement);
     this.controls.enableDamping = enableDamping;
     this.controls.dampingFactor = dampingFactor;
+    this.controls.maxPolarAngle = Math.PI / 2;
     this.controls.target.copy(target);
     this.controls.update();
 
